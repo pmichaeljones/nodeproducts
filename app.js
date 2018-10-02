c668783d71ec31b66d91b999a34de9800cd54435
Patrick Jones <pmichaeljones@gmail.com>
2018-10-02 15:11:11 +0200

    fixed typo on server instructions

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const app = express();
 let port = 1234;
 
 app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+    console.log('Server is up and running on port number ' + port);
 });
